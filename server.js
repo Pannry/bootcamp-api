1bd8bb49a7c482129432357431706f5a483d59c2
Pannry <thalestnd@gmail.com>
2019-10-26 11:46:04 -0300

    add routes

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'show all bootcamps' })
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` })
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'create new bootcamp' })
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}` })
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `delete bootcamp ${req.params.id}` })
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server running on port ${process.env.NODE_ENV} mode on port ${PORT}`));
