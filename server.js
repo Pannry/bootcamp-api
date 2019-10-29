4d2ab26a272e363ea9be413659325db336489a44
Pannry <thalestnd@gmail.com>
2019-10-29 11:02:21 -0300

    Add monog db

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./src/config/db');
+
+// carregando env vars
+dotenv.config({ path: './src/config/config.env' });
+
+connectDB();
 
 // Route file
 const bootcamps = require('./src/routes/bootcamps')
 
-// carregando env vars
-dotenv.config({path: './src/config/config.env'});
-
 const app = express();
 
 // dev loggin middleware
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
@@ -20,4 +23,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandle promises rejections
+process.on('unhandledRejection', (err, promisse) => {
+  console.log(`error: ${err.message}`);
+  // Close aplication on error
+  server.close(() => process.exit(1));
+})
