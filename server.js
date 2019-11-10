76c0f0946d2e65f143fb64b1dd62b8437ada9601
Pannry <thalestnd@gmail.com>
2019-11-10 12:28:41 -0300

    concluindo basico do mongoose

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const bootcamps = require('./src/routes/bootcamps')
 
 const app = express();
 
+//body parser
+app.use(express.json())
+
 // dev loggin middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
